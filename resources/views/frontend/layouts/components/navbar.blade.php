c061379a436ca7941bbf226cab2fdc5234974fc2
idlkz <idlkz2019@gmail.com>
2020-07-10 12:38:22 +0600

    first commit

--- resources/views/frontend/layouts/components/navbar.blade.php
+++ resources/views/frontend/layouts/components/navbar.blade.php
@@ -21,110 +21,24 @@
 
                 <div class="collapse navbar-collapse mean-menu">
                     <ul class="navbar-nav">
-                        <li class="nav-item"><a href="index-2.html#" class="nav-link active">Home <i class="flaticon-down-arrow"></i></a>
+
+                        <li class="nav-item"><a href="index-2.html#" class="nav-link">Новости </a></li>
+
+                        <li class="nav-item"><a href="index-2.html#" class="nav-link">Статистика <i class="flaticon-down-arrow"></i></a>
                             <ul class="dropdown-menu">
-                                <li class="nav-item"><a href="index.html" class="nav-link">Main Home</a></li>
-
-                                <li class="nav-item"><a href="index-2.html" class="nav-link active">Esports Home</a></li>
-
-                                <li class="nav-item"><a href="index-3.html" class="nav-link">Bosting Home</a></li>
-
-                                <li class="nav-item"><a href="index-4.html" class="nav-link">Magazine Home</a></li>
-
-                                <li class="nav-item"><a href="index-5.html" class="nav-link">Tournaments Home</a></li>
-
-                                <li class="nav-item"><a href="index-6.html" class="nav-link">Streaming Home</a></li>
+                                <li class="nav-item"><a href="stream-schedule.html" class="nav-link">Турниры</a></li>
+                                <li class="nav-item"><a href="single-tournament.html" class="nav-link">Команды</a></li>
                             </ul>
                         </li>
 
-                        <li class="nav-item"><a href="index-2.html#" class="nav-link">Pages <i class="flaticon-down-arrow"></i></a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a href="team.html" class="nav-link">Our Team</a></li>
+                        <li class="nav-item"><a href="index-2.html#" class="nav-link">Участие в турнирах</a></li>
 
-                                <li class="nav-item"><a href="sponsors.html" class="nav-link">Our Sponsors</a></li>
+                        <li class="nav-item"><a href="index-2.html#" class="nav-link">Поиск команд </a></li>
 
-                                <li class="nav-item"><a href="gallery.html" class="nav-link">Gallery</a></li>
-
-                                <li class="nav-item"><a href="faq.html" class="nav-link">FAQ Page</a></li>
-
-                                <li class="nav-item"><a href="coming-soon.html" class="nav-link">Coming Soon</a></li>
-
-                                <li class="nav-item"><a href="error-404.html" class="nav-link">404 Error Page</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item"><a href="index-2.html#" class="nav-link">Tournament <i class="flaticon-down-arrow"></i></a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a href="stream-schedule.html" class="nav-link">Stream Schedule</a></li>
-
-                                <li class="nav-item"><a href="single-tournament.html" class="nav-link">Tournament Single</a></li>
-
-                                <li class="nav-item"><a href="single-match.html" class="nav-link">Match Single</a></li>
-
-                                <li class="nav-item"><a href="single-team.html" class="nav-link">Team Single</a></li>
-
-                                <li class="nav-item"><a href="single-player.html" class="nav-link">Player Single</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item"><a href="index-2.html#" class="nav-link">Shop <i class="flaticon-down-arrow"></i></a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a href="products-list-1.html" class="nav-link">Products List 01</a></li>
-
-                                <li class="nav-item"><a href="products-list-2.html" class="nav-link">Products List 02</a></li>
-
-                                <li class="nav-item"><a href="cart.html" class="nav-link">Cart</a></li>
-
-                                <li class="nav-item"><a href="checkout.html" class="nav-link">Checkout</a></li>
-
-                                <li class="nav-item"><a href="single-products.html" class="nav-link">Products Details</a></li>
-
-                                <li class="nav-item"><a href="my-account.html" class="nav-link">My Account</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item"><a href="index-2.html#" class="nav-link">Blog <i class="flaticon-down-arrow"></i></a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a href="blog-1.html" class="nav-link">Grid (2 in Row)</a></li>
-
-                                <li class="nav-item"><a href="blog-2.html" class="nav-link">Grid (3 in Row)</a></li>
-
-                                <li class="nav-item"><a href="blog-3.html" class="nav-link">Masonry (3 in Row)</a></li>
-
-                                <li class="nav-item"><a href="blog-4.html" class="nav-link">Right Sidebar</a></li>
-
-                                <li class="nav-item"><a href="blog-5.html" class="nav-link">Left Sidebar</a></li>
-
-                                <li class="nav-item"><a href="index-2.html#" class="nav-link">Single Post <i class='bx bx-plus'></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item"><a href="single-blog-1.html" class="nav-link">Default</a></li>
-
-                                        <li class="nav-item"><a href="single-blog-2.html" class="nav-link">With Video</a></li>
-
-                                        <li class="nav-item"><a href="single-blog-3.html" class="nav-link">With Image Slider</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+                        <li class="nav-item"><a href="contact.html" class="nav-link">Контакты</a></li>
                     </ul>
 
                     <div class="others-option d-flex align-items-center">
-                        <div class="option-item">
-                            <div class="cart-btn">
-                                <a href="cart.html">
-                                    <i class="flaticon-null-5"></i>
-                                    <span>1</span>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="option-item">
-                            <div class="search-box">
-                                <i class="flaticon-search-1"></i>
-                            </div>
-                        </div>
 
                         <div class="option-item">
                             <div class="side-menu-btn">
@@ -139,7 +53,7 @@
 
     <div class="others-option-for-responsive">
         <div class="container">
-            <div class="dot-menu">
+            <div class="dot-menu" data-toggle="modal" data-target="#sidebarModal">
                 <div class="inner">
                     <div class="circle circle-one"></div>
                     <div class="circle circle-two"></div>
@@ -147,32 +61,18 @@
                 </div>
             </div>
 
-            <div class="container">
-                <div class="option-inner">
-                    <div class="others-option">
-                        <div class="option-item">
-                            <div class="cart-btn">
-                                <a href="index-2.html#">
-                                    <i class="flaticon-null-5"></i>
-                                    <span>1</span>
-                                </a>
-                            </div>
-                        </div>
+{{--            <div class="container">--}}
+{{--                <div class="option-inner">--}}
+{{--                    <div class="others-option">--}}
 
-                        <div class="option-item">
-                            <div class="search-box">
-                                <i class="flaticon-search-1"></i>
-                            </div>
-                        </div>
-
-                        <div class="option-item">
-                            <div class="side-menu-btn">
-                                <i class="flaticon-null-2" data-toggle="modal" data-target="#sidebarModal"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+{{--                        <div class="option-item">--}}
+{{--                            <div class="side-menu-btn">--}}
+{{--                                <i class="flaticon-null-2" data-toggle="modal" data-target="#sidebarModal"></i>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 </div>
