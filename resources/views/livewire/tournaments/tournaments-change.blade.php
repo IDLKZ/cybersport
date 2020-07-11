2144f2095839a7b79b000cb34ec4f0c7f76059fa
idlkz2019 <idlkz2019@gmail.com>
2020-07-11 15:03:22 +0600

    09/07 2020 migrations

--- resources/views/livewire/tournaments/tournaments-change.blade.php
+++ resources/views/livewire/tournaments/tournaments-change.blade.php
@@ -17,7 +17,7 @@
                 <select class="form-control" name="type" wire:model = "game_id">
                     <option value="{{$game_id2}}">{{$game_id2_title}}</option>
                     @foreach($games as $game)
-                        @if($game->id != $game_id2)
+                        @if($game->id !== $game_id2)
                             <option value="{{$game->id}}">{{$game->title}}</option>
                         @endif
                     @endforeach
